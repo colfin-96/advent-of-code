0bcc51d8aa943e9766cced6807e27eddbc834f31
Colin Finger <finger.colin@gmail.com>
2022-02-25 22:20:58 +0100

    added 2021/01 solution

--- 2021/day01.ts
+++ 2021/day01.ts
@@ -1,1 +1,22 @@
-console.log('test');
+import { FileLoader } from '../services/fileLoader';
+
+// const testInput = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+let fl: FileLoader = new FileLoader();
+let testInput: number[] = fl.load(`${__dirname}/input.txt`);
+
+let increasements = 0;
+let lastNumber: number;
+
+testInput.forEach((currentNumber, index) => {
+  if (index === 0) {
+    lastNumber = currentNumber;
+    return;
+  }
+
+  if (currentNumber > lastNumber) {
+    increasements++;
+  }
+  lastNumber = currentNumber;
+});
+
+console.log(increasements);
