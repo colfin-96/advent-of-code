e516a1a3c12b3f1701b43013ac0711d4bac3ab9c
Colin Finger <finger.colin@gmail.com>
2022-02-27 11:18:28 +0100

    unfinished 2021/03.2

--- 2021/day03.ts
+++ 2021/day03.ts
@@ -1,18 +1,24 @@
 import { FileLoader } from '../services/fileLoader';
 
-// const testInput = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+// Result 1
 let fl: FileLoader = new FileLoader();
 let testInput: number[][] = fl
   .load(`${__dirname}/day03.txt`)
   .map((x: string) => {
-    const arr = x.split('').map((y) => {
-      return parseInt(y, 10);
-    });
+    const arr = x
+      .split('')
+      .map((y) => {
+        const current = parseInt(y, 10);
+        return current;
+      })
+      .filter((x) => {
+        return !isNaN(x);
+      });
     return arr;
   });
 
-let mostCommonBits: number[] = [];
-let leastCommonBits: number[] = [];
+let mostCommonBits: number[] = [],
+  leastCommonBits: number[] = [];
 
 for (let x = 0; x < testInput[0].length; x++) {
   let countZero = 0;
@@ -44,3 +50,48 @@ leastCommonBits.forEach((x, index) => {
 });
 
 console.log('Result 1: ' + gamma * epsilon);
+
+// Result 2
+let bitPosition = 0,
+  zeroes: number[][] = [],
+  ones: number[][] = [];
+
+let resultingLines: number[] = [];
+getSomething(testInput, true);
+let oxygenGeneratorRating = convertToBinary(resultingLines);
+console.log(oxygenGeneratorRating);
+
+resultingLines = [];
+bitPosition = 0;
+getSomething(testInput, false);
+let co2ScrubberRating = convertToBinary(resultingLines);
+console.log(co2ScrubberRating);
+
+// Functions
+function getSomething(remainingLines: number[][], findMore: boolean) {
+  remainingLines.forEach((line) => {
+    line[bitPosition] === 0 ? zeroes.push(line) : ones.push(line);
+  });
+  if (zeroes.length > ones.length) {
+    remainingLines = findMore ? zeroes : ones;
+  } else if (zeroes.length <= ones.length) {
+    remainingLines = findMore ? ones : zeroes;
+  }
+  bitPosition++;
+  zeroes = [];
+  ones = [];
+  if (bitPosition === testInput[0].length + 1) {
+    resultingLines = remainingLines[0];
+    return;
+  }
+  getSomething(remainingLines, findMore);
+}
+
+function convertToBinary(lines: number[]) {
+  let result = 0;
+  lines.forEach((x, index) => {
+    const current = Math.pow(2, lines.length - index - 1) * x;
+    result += current;
+  });
+  return result;
+}
